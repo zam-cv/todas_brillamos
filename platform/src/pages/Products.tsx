998c3a9caab6cfef13fd2981d4aa46f1327a78a5
SsebasA <sebastian.antonio1707@gmail.com>
2024-08-28 16:50:48 -0600

    Added Input element and mock table

--- platform/src/pages/Products.tsx
+++ platform/src/pages/Products.tsx
@@ -1,3 +1,51 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 export default function Products() {
-  return <div>Products</div>
+  return(
+    <div>
+      Products
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input type= "search" placeholder = "search" />
+        <Button type= "submit">Añadir Producto</Button>
+      </div>
+
+
+
+      <Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Invoice</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Method</TableHead>
+      <TableHead className="text-right">Amount</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+
+      
+    </div>
+  ) 
+        
+  
+  
 }
