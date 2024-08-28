4a0c9aca2306ed84505ad53269ceae9c3ee505c5
Carlos Zamudio <a01799283@tec.mx>
2024-08-27 20:39:01 -0600

    add-platform: shadcn

--- platform/src/App.tsx
+++ platform/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
