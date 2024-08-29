136ff37f3f871f5df9479a3eb6dc6e5bdf67d079
SsebasA <sebastian.antonio1707@gmail.com>
2024-08-29 08:00:24 -0600

    Dashboard (in progress)

--- platform/src/pages/Specialists.tsx
+++ platform/src/pages/Specialists.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button"
+import { IoIosAddCircleOutline } from "react-icons/io";
+
+
 export default function Specialists() {
   return (
     <div>
        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           Especialistas
       </h2>   
+      <br></br>
+      <Button>
+        <IoIosAddCircleOutline className="mr-2 h-4 w-4"/>Añadir Especialista
+      </Button>
       
     </div>
   )
