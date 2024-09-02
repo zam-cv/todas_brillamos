3f973b9a0b9c4a77c15775a58ccf651f789f7e29
SsebasA <sebastian.antonio1707@gmail.com>
2024-09-02 13:50:09 -0600

    Products table updated

--- platform/src/components/table/components/columns-products.tsx
+++ platform/src/components/table/components/columns-products.tsx
@@ -50,6 +50,125 @@ export const columns: ColumnDef<Task>[] = [
         );
       },
     },
-    
+    {
+      accessorKey: "precio",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Precio" />
+      ),
+      cell: ({ row }) => {
+        return (
+          <div className="flex space-x-2">
+            <span className="max-w-[500px] truncate font-medium">
+              {row.getValue("precio")}
+            </span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "marca",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Marca" />
+      ),
+      cell: ({ row }) => {
+        return (
+          <div className="flex space-x-2">
+            <span className="max-w-[500px] truncate font-medium">
+              {row.getValue("marca")}
+            </span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "tamano",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Tamaño" />
+      ),
+      cell: ({ row }) => {
+        return (
+          <div className="flex space-x-2">
+            <span className="max-w-[500px] truncate font-medium">
+              {row.getValue("tamano")}
+            </span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "material",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Material" />
+      ),
+      cell: ({ row }) => {
+        return (
+          <div className="flex space-x-2">
+            <span className="max-w-[500px] truncate font-medium">
+              {row.getValue("material")}
+            </span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "absorbencia",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Absorbencia" />
+      ),
+      cell: ({ row }) => {
+        return (
+          <div className="flex space-x-2">
+            <span className="max-w-[500px] truncate font-medium">
+              {row.getValue("absorbencia")}
+            </span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "cuidadoPiel",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Cuidado de la Piel" />
+      ),
+      cell: ({ row }) => {
+        return (
+          <div className="flex space-x-2">
+            <span className="max-w-[500px] truncate font-medium">
+              {row.getValue("cuidadoPiel")}
+            </span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "color",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Color" />
+      ),
+      cell: ({ row }) => {
+        return (
+          <div className="flex space-x-2">
+            <span className="max-w-[500px] truncate font-medium">
+              {row.getValue("color")}
+            </span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "descripcion",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Descripción" />
+      ),
+      cell: ({ row }) => {
+        return (
+          <div className="flex space-x-2">
+            <span className="max-w-[500px] truncate font-medium">
+              {row.getValue("descripcion")}
+            </span>
+          </div>
+        );
+      },
+    },
   ];
   
--- platform/src/pages/Products.tsx
+++ platform/src/pages/Products.tsx
@@ -15,7 +15,17 @@ import {
 
 export default function Products() {
   const mockData = [
-    { idProducto: 'XDcaw1', nombreProd: 'Producto #1', cantidad: '12' }
+    { idProducto: 'XDcaw1', 
+      nombreProd: 'Producto #1', 
+      cantidad: '12' , 
+      precio: '12.00', 
+      marca: 'Marca #1', 
+      tamano: '12x12', 
+      material: 'Material #1', 
+      absorbencia: '12', 
+      cuidadoPiel: 'Cuidado #1', 
+      color: 'Color #1', 
+      descripcion: 'Descripcion #1' },
   ];
 
 
@@ -27,7 +37,7 @@ export default function Products() {
     <br></br>
     <Accordion type="single" collapsible className="w-3/4">
         <AccordionItem value="item-1">
-          <AccordionTrigger>Agregar</AccordionTrigger>
+          <AccordionTrigger className="font-semibold">Agregar</AccordionTrigger>
             <AccordionContent>
               <div className = "flex flex-nowrap flex-col">
                   <div className="flex flex-row space-x-2 px-2 pt-2">
@@ -63,11 +73,14 @@ export default function Products() {
 
     <br></br>
 
-    <DataTable
+    <div>   
+       <DataTable
       data = {mockData}
       columns = {columns}
       event_id = {1}
     />
+    </div>
+
       
       
     </div>
