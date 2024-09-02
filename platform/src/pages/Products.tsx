fdee4c59cb07c4ca8df92ad53280f2bd51597456
SsebasA <sebastian.antonio1707@gmail.com>
2024-09-02 13:24:41 -0600

    Form to add Products added

--- platform/src/pages/Products.tsx
+++ platform/src/pages/Products.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button"
 import { columns } from "@/components/table/components/columns-products";
 import { DataTable } from "@/components/table/components/data-table-products";
 import { IoIosAdd, IoIosAddCircleOutline } from "react-icons/io";
+import { Textarea } from "@/components/ui/textarea"
+
 import {
   Accordion,
   AccordionContent,
@@ -23,15 +25,35 @@ export default function Products() {
           Productos
       </h2>
     <br></br>
-    <Accordion type="single" collapsible>
+    <Accordion type="single" collapsible className="w-3/4">
         <AccordionItem value="item-1">
           <AccordionTrigger>Agregar</AccordionTrigger>
             <AccordionContent>
-              <div className = "flex flex-row flex-nowrap w-3/5 space-x-2.5">
-                  <Input placeholder="Nombre del producto"></Input>
-                  <Input placeholder="Cantidad"></Input>
-                  <Input placeholder="Precio"></Input>
-
+              <div className = "flex flex-nowrap flex-col">
+                  <div className="flex flex-row space-x-2 px-2 pt-2">
+                    <Input className="w-4/5" placeholder="Nombre del producto"></Input>
+                    <Input className="w-3/4 " placeholder="Cantidad"></Input>
+                  </div>
+                  <div className="flex flex-row space-x-2 px-2 pt-2">
+                    <Input placeholder="Precio"></Input>
+                    <Input placeholder="Marca"></Input>
+                  </div>
+                  <div className="flex flex-row space-x-2 px-2 pt-2">
+                    <Input placeholder="Tamaño"></Input>
+                    <Input placeholder="Precio"></Input>
+                    <Input placeholder="Material"></Input>
+                  </div>
+                  <div className="flex flex-row space-x-2 px-2 pt-2">
+                    <Input placeholder="Absorbencia"></Input>
+                    <Input placeholder="Cuidado de la piel"></Input>
+                    <Input placeholder="Color"></Input>
+                  </div>
+                  <div className="flex flex-row space-x-2 px-2 pt-2">
+                      <Textarea placeholder="Descripción.."/>
+                  </div>
+                  <div className="flex flex-row space-x-2 px-2 pt-2">
+                      <Button>Agregar</Button>
+                  </div>
               </div>
             </AccordionContent>
         </AccordionItem>
