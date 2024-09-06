e69bfb3b76cab28cc3e91e9272b8f29b074f6897
A01749898 <A01749898@tec.mx>
2024-09-06 12:29:08 -0600

    Cards updated

--- platform/src/components/CardsDashboard.tsx
+++ platform/src/components/CardsDashboard.tsx
@@ -23,13 +23,13 @@ export default function CardsDashboard() {
         <div>
             <Card x-chunk="dashboard-01-chunk-0">
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">Ganancias totales</CardTitle>
+                <CardTitle className="text-sm font-medium">Ventas totales</CardTitle>
                 <DollarSign className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-                <div className="text-2xl font-bold">$35,231.89</div>
+                <div className="text-2xl font-bold">450</div>
                 <p className="text-xs text-muted-foreground">
-                Ganancias del último trimestre
+                Ventas del último trimestre
                 </p>
             </CardContent>
             </Card>
