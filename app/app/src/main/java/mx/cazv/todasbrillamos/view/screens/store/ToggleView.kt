d5fe0be8f73ccfda04e3bebac28ce7a03f4d3b43
A01750338 <117104168+A01750338@users.noreply.github.com>
2024-09-07 22:48:18 -0600

    add-app: toggle view

---
+++ app/app/src/main/java/mx/cazv/todasbrillamos/view/screens/store/ToggleView.kt
@@ -0,0 +1,74 @@
+package mx.cazv.todasbrillamos.view.screens.store
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import mx.cazv.todasbrillamos.R
+
+
+@Composable
+fun ToggleView(selectedType: String, onSelectionChange: (String) -> Unit) {
+    val gridFill = R.drawable.ic_grid_view_fill
+    val gridEmpty = R.drawable.ic_grid_view
+    val listFill = R.drawable.ic_list_view_fill
+    val listEmpty = R.drawable.ic_list_view
+
+    Surface(
+        shape = RoundedCornerShape(24.dp),
+        modifier = Modifier.wrapContentSize()
+    ) {
+        Row (modifier = Modifier
+            .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        )
+        {
+            Text(text = "Productos",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Left ),
+                modifier = Modifier.padding(start = 10.dp)
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            // Grid Icon
+            Icon(
+                painter = painterResource(
+                    id = if (selectedType == "grid") gridFill else gridEmpty
+                ),
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable { onSelectionChange("grid") }
+                    .padding(end = 5.dp)
+            )
+
+            // List Icon
+            Icon(
+                painter = painterResource(
+                    id = if (selectedType == "list") listFill else listEmpty
+                ),
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable { onSelectionChange("list") }
+                    .padding(end = 12.dp)
+            )
+        }
+    }
+}
---
+++ app/app/src/main/java/mx/cazv/todasbrillamos/view/screens/store/ViewProducts.kt
@@ -0,0 +1,45 @@
+package mx.cazv.todasbrillamos.view.screens.store
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import mx.cazv.todasbrillamos.model.ProductProvider
+
+
+@Composable
+fun ViewProducts(type: String, modifier: Modifier = Modifier) {
+    if (type == "grid") {
+        LazyVerticalGrid(
+            // columns = GridCells.Adaptive(160.dp),
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 16.dp),
+        ) {
+            itemsIndexed(ProductProvider.productList) { index, product ->
+                ProductGridItem(product = product)
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+    }
+    else if (type == "list") {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 16.dp),
+        ) {
+            itemsIndexed(ProductProvider.productList) { index, product ->
+                ProductColumnItem(product = product)
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+    }
+
+}
