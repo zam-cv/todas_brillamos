df5649e6e61012bfc11440222d65d35ae2d3d1f8
SsebasA <sebastian.antonio1707@gmail.com>
2024-09-17 11:07:26 -0600

    client model validations

--- backend/src/models/client.go
+++ backend/src/models/client.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"errors"
+	"regexp"
+
 	"gorm.io/gorm"
 )
 
@@ -11,5 +14,31 @@ type Client struct {
 	UserID    uint
 }
 
+// Función para validar nombre y apellido
+func ValidateName(FirstName string) error {
+	// Expresión regular para permitir solo letras y algunos caracteres especiales comunes
+	nameRegex := regexp.MustCompile(`^[a-zA-ZÀ-ÿ\s]{2,50}$`)
 
-func Validatre
+	// Verificar longitud del nombre
+	if len(FirstName) < 2 || len(FirstName) > 50 {
+		return errors.New("Nombre inválido")
+	}
+
+	// Verificar que el nombre cumpla con la expresión regular
+	if !nameRegex.MatchString(FirstName) {
+		return errors.New("Nombre inválido")
+	}
+
+	return nil
+}
+
+func ValidateLastName(LastName string) error {
+	lastNameRegex := regexp.MustCompile(`^[a-zA-ZÀ-ÿ\s]{2,50}$`)
+	if len(LastName) < 2 || len(LastName) > 50 {
+		return errors.New("Nombre inválido")
+	}
+	if !lastNameRegex.MatchString(LastName) {
+		return errors.New("Nombre inválido")
+	}
+	return nil
+}
