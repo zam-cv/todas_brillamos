4fc75984e8c2ec5cac507c6ddadb2c74134ae4e1
SsebasA <sebastian.antonio1707@gmail.com>
2024-09-19 12:55:50 -0600

    querys on others.go

--- backend/database/others.go
+++ backend/database/others.go
@@ -4,15 +4,10 @@ import (
 	"backend/models"
 )
 
-func InsertOtherData(other *models.Other) (uint, error) {
-	db := GetDatabase()
-	tx := db.Begin()
-	if err := tx.Create(&other).Error; err != nil {
-		tx.Rollback()
-		return 0, err
-	}
+func InsertOnOtherByClientID(other *models.Other) (uint, error) {
 
-	tx.Commit()
-	return other.ID, nil
+}
+
+func ModifyDataOnOtherByClientID(other *models.Other) error {
 
 }
