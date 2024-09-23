3bb759a0ad4a22f0e745c2e2fc263adc51701221
SsebasA <sebastian.antonio1707@gmail.com>
2024-09-23 07:22:23 -0600

    fix: Table on products page

--- platform/src/components/table/components/data-table-products.tsx
+++ platform/src/components/table/components/data-table-products.tsx
@@ -119,9 +119,8 @@ export function DataTable<TData, TValue>({
             )}
           </TableBody>
         </Table>
-      </div>
-
-      {/* <DataTablePagination table={table} /> */}
+        <DataTablePagination table={table} />
+      </div>      
     </div>
   );
 }
