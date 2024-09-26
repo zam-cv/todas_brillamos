35567d5236a28e33f09562c5310a17b23c11b3b7
SsebasA <sebastian.antonio1707@gmail.com>
2024-09-26 08:38:02 -0600

    Add:  article form in "Agregar" page

--- platform/src/pages/Specialists.tsx
+++ platform/src/pages/Specialists.tsx
@@ -1,4 +1,13 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 import { IoIosAddCircleOutline } from "react-icons/io";
 
 
@@ -9,7 +18,7 @@ export default function Specialists() {
           Especialistas
       </h2>   
       <br></br>
-      <div className="absolute overflow-auto flex flex-col gap-5 ">
+      <div className="overflow-auto gap-5 ">
           <div>
             <Button>
               <IoIosAddCircleOutline className="mr-2 h-4 w-4"/>Añadir Especialista
@@ -26,9 +35,24 @@ export default function Specialists() {
             </Button>
           </div>
           <div>
-            <Button>
-              <IoIosAddCircleOutline className="mr-2 h-4 w-4"/>Añadir Artículo
-            </Button>
+            <Accordion type="single" collapsible className="w-3/4">
+               <AccordionItem value="item-4">
+                <AccordionTrigger className="font-semibold">Agregar Artículo</AccordionTrigger>
+                  <AccordionContent>
+                      <div className="flex flex-nowrap flex-col">
+                        <div className="flex flex-row space-x-2 px-2 pt-2">
+                          <Input name="nombreArt" value= "" placeholder="Nombre del artículo"></Input>
+                        </div>
+                        <div className="flex flex-row space-x-2 px-2 pt-2">
+                           <Input name="author" value ="" placeholder="Autor"></Input>
+                        </div>
+                        <div className="flex flex-row space-x-2 px-2 pt-2">
+                          <Textarea name="articulo" value="" placeholder="Artículo..."></Textarea>
+                        </div>
+                      </div>
+                  </AccordionContent>
+               </AccordionItem>
+            </Accordion>
           </div>
        
       </div>
