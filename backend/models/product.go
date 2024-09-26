b8f6ec9f5bbffd345d495a126415ae76860e5afc
A01750338 <117104168+A01750338@users.noreply.github.com>
2024-09-26 13:33:29 -0600

    update-backend: product details

--- backend/models/product.go
+++ backend/models/product.go
@@ -1,21 +1,48 @@
 package models
 
 type Product struct {
-	ID          uint    `json:"-" gorm:"primarykey"`
+	ID uint `json:"-" gorm:"primarykey"`
+
+	// Image
+	Hash string `json:"hash"`
+	Type string `json:"type"`
+
+	// General
+	Model       string  `json:"model"`
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
 	Stock       int     `json:"stock"`
-	Hash        string  `json:"hash"`
-	Type        string  `json:"type"`
-	CategoryID  uint    `json:"category_id"`
+
+	// Specific
+	Size        string `json:"size"`
+	Color       string `json:"color"`
+	Maintenance string `json:"maintenance"`
+	Material    string `json:"material"`
+	Absorbency  string `json:"absorbency"`
+	SkinCare    string `json:"skin_care"`
+
+	// Relationships
+	CategoryID uint `json:"category_id"`
 }
 
 type ProductMetadata struct {
+	// General
+	Model       string  `json:"model" validate:"required,min=1"`
 	Name        string  `json:"name" validate:"required,min=1"`
 	Description string  `json:"description" validate:"required"`
 	Price       float64 `json:"price" validate:"required,min=0"`
 	Stock       int     `json:"stock" validate:"required,min=0"`
+
+	// Specific
+	Size        string  `json:"size" validate:"required"`
+	Color       string  `json:"color" validate:"required"`
+	Maintenance string  `json:"maintenance" validate:"required"`
+	Material    string  `json:"material" validate:"required"`
+	Absorbency  string  `json:"absorbency" validate:"required"`
+	SkinCare    string  `json:"skin_care" validate:"required"`
+
+	// Relationships
 	CategoryID  uint    `json:"category_id" validate:"required,min=0"`
 }
 
@@ -36,9 +63,16 @@ func (p *Product) SetMetadata(metadata *ProductMetadata) {
 		return
 	}
 
+	p.Model = metadata.Model
 	p.Name = metadata.Name
 	p.Description = metadata.Description
 	p.Price = metadata.Price
 	p.Stock = metadata.Stock
-	p.CategoryID = metadata.CategoryID
+	p.Size = metadata.Size
+	p.Color = metadata.Color
+	p.Maintenance = metadata.Maintenance
+	p.Material = metadata.Material
+	p.Absorbency = metadata.Absorbency
+	p.SkinCare = metadata.SkinCare
+	p.CategoryID = metadata.CategoryID	
 }
