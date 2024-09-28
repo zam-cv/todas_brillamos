cb373f51a4bc716d306b4966b2490837d3ff5310
SsebasA <sebastian.antonio1707@gmail.com>
2024-09-28 13:37:24 -0600

    Add: API Product

--- platform/api/products.ts
+++ platform/api/products.ts
@@ -26,6 +26,28 @@ export default {
             return upload("admin/products/upload", file, product, false);
         },
 
+        updateMetadataProduct : (
+            file: File,
+            product: Product
+        ): Promise<void> => {
+            return upload("admin/products/1/metadata", file, product, false);
+        },
+
+        addProductToCart: (
+            file:File,
+            product: Product
+        ): Promise<void> => {
+            return upload("admin/cart/2/3", file, product, false)
+        },
+
+        addProductToFavorites: (
+            file: File,
+            product: Product
+        ): Promise<void> => {
+            return upload("admin/favorites/1", file, product, false)
+        },
+        
+        //GET
         getProducts: (): Promise<void> => {
             return get("admin/products")
         },
@@ -36,9 +58,39 @@ export default {
 
         getProductFromCart: (): Promise<void> => {
             return get("admin/products/{cart}")
+        },
+
+        getProductFromFavorites: (): Promise<void> => {
+            return get("admin/favorites")
+        },
+
+        //PUT
+        updateProduct: (
+            file: File,
+            product: Product
+        ): Promise<void> => {
+            return upload("admin/products/1", file, product, false);
+        },
+
+        updateProdQuantityInCart: (
+            file: File,
+            product: Product
+        ): Promise<void> => {
+            return upload("admin/cart/1/1", file, product, false)
+        },
+
+        //DEL
+        deleteProduct: (): Promise<void> => {
+            return del("admin/products/2")
+        },
+
+        deleteProductFromCart: (): Promise<void> => {
+            return del("admin/cart/2")
+        },
+
+        deleteProductFromFavorites: (): Promise<void> => {
+            return del("admin/favorites/1")
         }
-
-
 
     }
 }
