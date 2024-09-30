5eb97fd597c0b976f524f1ee77a90abbda935f9c
SsebasA <sebastian.antonio1707@gmail.com>
2024-09-30 10:33:51 -0600

    Add: Favorites on API

--- platform/api/favorites.ts
+++ platform/api/favorites.ts
@@ -1,1 +1,31 @@
+import {get, del, post, upload} from "../src/utils/methods";
 
+export interface Favorites{
+    id: string;
+    client_id: string;
+    product_id: string;
+}
+
+
+export default {
+    favorites: {
+        getFavorites: (): Promise<void> => {
+            return get("admin/favorites")
+        },
+
+        getProductFromFavorites: (id: number): Promise<void> => {
+            return get(`admin/favorites/${id}`)
+        },
+
+        deleteProductFromFavorites: (id: number): Promise<void> => {
+            return del(`admin/favorites/${id}`)
+        },
+
+        addProductToFavorites: (
+            favorites: Favorites
+        ): Promise<void> => {
+            return post("admin/favorites", favorites)
+        }
+        
+    }
+}
