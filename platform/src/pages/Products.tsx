c39296f874ca2beda1a150b7ae4ef481b93ffb57
SsebasA <sebastian.antonio1707@gmail.com>
2024-10-04 16:33:38 -0600

    Update: Select category selector

--- platform/src/pages/Products.tsx
+++ platform/src/pages/Products.tsx
@@ -4,9 +4,9 @@ import { columns } from "@/components/table/components/columns-products";
 import { DataTable } from "@/components/table/components/data-table-products";
 import { Textarea } from "@/components/ui/textarea";
 import { useEffect, useState, useRef } from "react";
-import api, {Product} from "@/utils/api/products";
+import api, { Product } from "@/utils/api/products";
 import apiCategory, { Category } from "@/utils/api/category";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { SERVER } from "@/utils/constants";
 
 import {
@@ -22,23 +22,23 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
 import { SelectGroup, SelectLabel } from "@radix-ui/react-select";
 
-  // {
-  //   "model": "0017",
-  //   "name": "prod23",
-  //   "description": "descripcion",
-  //   "price": 20,
-  //   "stock": 4,
-  //   "size": "21 x 7 cm",
-  //   "color": "negro",
-  //   "maintenance": "Lavable y reutilizable",
-  //   "material": "Algodón",
-  //   "absorbency": "Alta",
-  //   "material_feature": "Hipoalergénica, transpirable",
-  //   "category_id": 1
-  // }
+// {
+//   "model": "0017",
+//   "name": "prod23",
+//   "description": "descripcion",
+//   "price": 20,
+//   "stock": 4,
+//   "size": "21 x 7 cm",
+//   "color": "negro",
+//   "maintenance": "Lavable y reutilizable",
+//   "material": "Algodón",
+//   "absorbency": "Alta",
+//   "material_feature": "Hipoalergénica, transpirable",
+//   "category_id": 1
+// }
 
 export default function UploadProducts() {
   const imageInput = useRef<HTMLInputElement>(null);
@@ -59,12 +59,35 @@ export default function UploadProducts() {
   const [isFormValid, setIsFormValid] = useState(false);
   const navigate = useNavigate();
   const [folder, setFolder] = useState<string>("");
-  
+
   useEffect(() => {
-    const formValid = model !== "" && name !== "" && description !== "" && price > 0 && stock > 0 
-    && category_id > 0 && size !== "" && color !== "" && maintenance !== "" && material !== "" && absorbency !== "" && material_feature !== "";
+    const formValid =
+      model !== "" &&
+      name !== "" &&
+      description !== "" &&
+      price > 0 &&
+      stock > 0 &&
+      category_id > 0 &&
+      size !== "" &&
+      color !== "" &&
+      maintenance !== "" &&
+      material !== "" &&
+      absorbency !== "" &&
+      material_feature !== "";
     setIsFormValid(formValid);
-  }, [model, name, description, price, stock, color,maintenance, material, absorbency,material_feature,category_id]);
+  }, [
+    model,
+    name,
+    description,
+    price,
+    stock,
+    color,
+    maintenance,
+    material,
+    absorbency,
+    material_feature,
+    category_id,
+  ]);
 
   function uploadProduct() {
     if (imageInput.current) {
@@ -73,90 +96,90 @@ export default function UploadProducts() {
         return;
       }
 
-      api.product.setProduct(file, {
-        model,
-        name,
-        description,
-        price,
-        stock,
-        size,
-        color,
-        maintenance,
-        material,
-        absorbency,
-        material_feature,
-        category_id,
-      } as any).then((data) => {
-        const id = data.id;
+      api.product
+        .setProduct(file, {
+          model,
+          name,
+          description,
+          price,
+          stock,
+          size,
+          color,
+          maintenance,
+          material,
+          absorbency,
+          material_feature,
+          category_id,
+        } as any)
+        .then((data) => {
+          const id = data.id;
 
-        setProducts([
-          ...products,
-          {
-            id,
-            model,
-            name,
-            description,
-            price,
-            stock,
-            size,
-            color,
-            maintenance,
-            material,
-            absorbency,
-            material_feature,
-            category_id,
-            url:`${SERVER}/${folder}/${data.name}`
-          } as any,
-        ])
+          setProducts([
+            ...products,
+            {
+              id,
+              model,
+              name,
+              description,
+              price,
+              stock,
+              size,
+              color,
+              maintenance,
+              material,
+              absorbency,
+              material_feature,
+              category_id,
+              url: `${SERVER}/${folder}/${data.name}`,
+            } as any,
+          ]);
 
-        // alert("Producto agregado");
-        setModel("");
-        setName("");
-        setDescription("");
-        setPrice(0);
-        setStock(0);
-        setSize("");
-        setColor("");
-        setMaintenance("");
-        setMaterial("");
-        setAbsorbency("");
-        setMaterial_feature("");
-        setCategory_id(0);
-        if (imageInput.current) {
-          imageInput.current.value = "";
-        }
-      })
-    }  
-}
+          // alert("Producto agregado");
+          setModel("");
+          setName("");
+          setDescription("");
+          setPrice(0);
+          setStock(0);
+          setSize("");
+          setColor("");
+          setMaintenance("");
+          setMaterial("");
+          setAbsorbency("");
+          setMaterial_feature("");
+          setCategory_id(0);
+          if (imageInput.current) {
+            imageInput.current.value = "";
+          }
+        });
+    }
+  }
 
-const[categories, setCategories] = useState<Category[]>([]);
-
+  const [categories, setCategories] = useState<Category[]>([]);
 
   useEffect(() => {
-      apiCategory.category.getCategories().then((categories) => {
-        console.log(categories);
-        setCategories(categories);
-      });
-  }, [])
-  
+    apiCategory.category.getCategories().then((categories) => {
+      console.log(categories);
+      setCategories(categories);
+    });
+  }, []);
 
   useEffect(() => {
     api.product.getProducts().then(([products, folder]) => {
       setProducts(products);
       setFolder(folder);
-    }); 
-  },[])
+    });
+  }, []);
 
   useEffect(() => {
     console.log(id);
-  }, [id])
+  }, [id]);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
       window.location.href = "/login";
     }
-  },[navigate]);
+  }, [navigate]);
 
   return (
     <div>
@@ -164,7 +187,6 @@ const[categories, setCategories] = useState<Category[]>([]);
         <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
           Productos
         </h2>
-  
       </div>
       <br />
       <Accordion type="single" collapsible className="w-3/4">
@@ -245,7 +267,7 @@ const[categories, setCategories] = useState<Category[]>([]);
                   name="maintenance"
                   value={maintenance}
                   onChange={(e) => setMaintenance(e.target.value)}
-                  placeholder="Manteinance"
+                  placeholder="Mantenimiento"
                 />
               </div>
               <div className="flex flex-row space-x-2 px-2 pt-2">
@@ -257,37 +279,35 @@ const[categories, setCategories] = useState<Category[]>([]);
                 />
               </div>
               <div className="flex flex-row space-x-2 px-2 pt-2">
-              <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Categoría" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Categorías</SelectLabel>
-                  {categories.map((category) => (
-                    <SelectItem 
-                      key={category.id} 
-                      value={category.id.toString()}
-                      onChange={() => setCategory_id(category.id)}
-                    >
-                      {category.name}
-                    </SelectItem>
-                   
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-                  <Input
-                    name="category_id"
-                    value={category_id}
-                    onChange={(e) => setCategory_id(Number(e.target.value))}
-                    placeholder="ID de categoría"
-                    type="number"
-                    required
-                  />
+                <Select
+                  value={category_id.toString()}
+                  onValueChange={(value) => setCategory_id(Number(value))}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Selecciona una categoría" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Categorías</SelectLabel>
+                      {categories.map((category) => (
+                        <SelectItem
+                          key={category.id}
+                          value={category.id.toString()}
+                        >
+                          {category.name}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
               </div>
               <div className="flex flex-row space-x-2 px-2 pt-2">
-                <Input ref={imageInput} type="file" onChange={() => {}} required />
+                <Input
+                  ref={imageInput}
+                  type="file"
+                  onChange={() => {}}
+                  required
+                />
               </div>
               <div className="flex flex-row space-x-2 px-2 pt-2">
                 <Button onClick={uploadProduct} disabled={!isFormValid}>
@@ -305,4 +325,3 @@ const[categories, setCategories] = useState<Category[]>([]);
     </div>
   );
 }
-
