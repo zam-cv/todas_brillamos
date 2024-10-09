8b784fe5bfab5f77832c10c75c6f95446c6df20a
A01749581 <117104241+A01749581@users.noreply.github.com>
2024-10-09 17:41:55 -0600

    fix: notifications

--- backend/database/notifications.go
+++ backend/database/notifications.go
@@ -21,15 +21,43 @@ func CreateNotification(notification *models.Notifications) error {
 }
 
 /*
- * Función que obtiene todas las notificaciones de un usuario de la base de datos
+ * Función que obtiene todas las notificaciones de un usuario de la base de datos y las arregla por fecha
  * @param clientID: ID del cliente
  * @return []models.Notifications: Arreglo de notificaciones
  * @return error: Error en caso de que exista
  */
-func GetNotificationsByClientID(clientID uint) ([]models.Notifications, error) {
+// func GetNotificationsByClientID(clientID uint) ([]models.Notifications, error) {
+// 	var notifications []models.Notifications
+// 	err := db.Where("client_id = ?", clientID).Find(&notifications).Error
+// 	return notifications, err
+// }
+func GetNotificationsByClientID(clientID uint) ([]models.GroupedNotifications, error) {
 	var notifications []models.Notifications
 	err := db.Where("client_id = ?", clientID).Find(&notifications).Error
-	return notifications, err
+	if err != nil {
+		return nil, err
+	}
+
+	grouped := make(map[string][]models.NotificationsGet)
+
+	for _, notification := range notifications {
+		dateKey := notification.Date.Format("2006-01-02") // Formato de la fecha
+		grouped[dateKey] = append(grouped[dateKey], models.NotificationsGet{
+			Title:       notification.Title,
+			Description: notification.Description,
+			ClientID:    notification.ClientID,
+		})
+	}
+
+	var result []models.GroupedNotifications
+	for date, notifs := range grouped {
+		result = append(result, models.GroupedNotifications{
+			Date:          date,
+			Notifications: notifs,
+		})
+	}
+
+	return result, nil
 }
 
 /*
