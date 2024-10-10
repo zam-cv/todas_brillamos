c95df84ac5846cd75724bcc553a037f88a2f4fba
Carlos Zamudio <a01799283@tec.mx>
2024-10-10 13:33:12 -0600

    update-app: ShippingInfo

--- app/app/src/main/java/mx/cazv/todasbrillamos/model/models/Others.kt
+++ app/app/src/main/java/mx/cazv/todasbrillamos/model/models/Others.kt
@@ -16,8 +16,8 @@ package mx.cazv.todasbrillamos.model.models
 data class Others(
     val CURP: String,
     val Street: String,
-    val Interior: String,
-    val Exterior: String,
+    val Interior: Int,
+    val Exterior: Int,
     val City: String,
     val State: String,
     val ZIP: String,
