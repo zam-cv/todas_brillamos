e2b6c266e87e931b121e9cf7ec80a6d019422861
Carlos Zamudio <a01799283@tec.mx>
2024-10-12 01:04:35 -0600

    update

--- backend/database/database.go
+++ backend/database/database.go
@@ -5,8 +5,6 @@
 package database
 
 import (
-	"fmt"
-	"log"
 	"sync"
 
 	"gorm.io/driver/postgres"
@@ -18,38 +16,38 @@ var once sync.Once
 
 // Inicializa la conexión a la base de datos.
 // Devuelve una instancia de *gorm.DB.
-func InitDatabase(host, port, user, password, name string) *gorm.DB {
-	once.Do(func() {
-		dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
-			host, port, user, password, name)
-
-		log.Printf("Attempting to connect to database with SSL enabled")
-
-		var err error
-		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-		if err != nil {
-			log.Fatalf("Failed to connect to database: %v", err)
-		}
-
-		log.Println("Successfully connected to database")
-	})
-
-	return db
-}
-
 // func InitDatabase(host, port, user, password, name string) *gorm.DB {
 // 	once.Do(func() {
-// 		dsn := "host=" + host + " port=" + port + " user=" + user + " dbname=" + name + " sslmode=disable password=" + password
+// 		dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+// 			host, port, user, password, name)
+
+// 		log.Printf("Attempting to connect to database with SSL enabled")
 
 // 		var err error
 // 		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 // 		if err != nil {
-// 			panic(err)
+// 			log.Fatalf("Failed to connect to database: %v", err)
 // 		}
+
+// 		log.Println("Successfully connected to database")
 // 	})
 
 // 	return db
 // }
+
+func InitDatabase(host, port, user, password, name string) *gorm.DB {
+	once.Do(func() {
+		dsn := "host=" + host + " port=" + port + " user=" + user + " dbname=" + name + " sslmode=disable password=" + password
+
+		var err error
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	return db
+}
 
 // Obtiene la instancia de la base de datos.
 // Pánico si la base de datos no está inicializada.
